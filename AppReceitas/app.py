ce3c5b4d3fbeee8d7f104235f5bf32a5c4eda450
ojuara-B <deividaraujope@gmail.com>
2021-10-17 17:44:46 -0300

    routes GET PUT DELETE

--- AppReceitas/app.py
+++ AppReceitas/app.py
@@ -20,10 +20,6 @@ receitas = [
    
 ]
 
-# @app.route("/cadastro1", methods=["GET"])
-# def Cadastro1():
-#    return receitas
-
 
 @app.route("/cadastro", methods=["POST","GET"])
 def Cadastro():
@@ -36,11 +32,50 @@ def Cadastro():
             "menssagem" : "Cadastrado",
             "newValue": newcadastro
 
-        })
+        }) 
    
+@app.route('/cadastro/<int:indice>', methods=['GET', 'PUT', 'DELETE'])
+def cadastroID(indice):
+    try:
+        receitas[indice]
+    except IndexError:
+        message = 'Receita ID {} Não Encontrada'.format(indice)
+        return jsonify({
+            "message": message,
+            "status": "Error!"
+        })
+    except:
+        message = 'Aconteceu um erro inesperado'
+        return jsonify({
+            "message": message,
+            "status": "Error!"
+        })
+    # Verifica se o método da requisição é GET
+    if request.method == 'GET':
+        return receitas[indice]
+    # Caso não seja GET verifica se é PUT
+    elif request.method == 'PUT':
+        # newValue recebe os dados do corpo da requisição e passa para o formato json
+        newValue = json.loads(request.data)
+        # Altera o valor da lista no indice informado com o novo valor recebido
+        receitas[indice] = newValue
+        return jsonify({
+            "message": "Updated!",
+            "newValue": newValue
+        })
+    elif request.method == 'DELETE':
+        print(indice)
+        receitas.pop(indice)
+        return jsonify({
+            "message": "Deleted!",
+            "arrayAtual": receitas
+        })
+
+
 if __name__ == '__main__':
     app.run(debug=True)
 
+  
 
 
 
