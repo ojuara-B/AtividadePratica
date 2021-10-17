6046d8b3ec409c959a7f4a50980ee0a3887f81f2
ojuara-B <deividaraujope@gmail.com>
2021-10-17 17:07:31 -0300

    Rota Cadastro

--- AppReceitas/app.py
+++ AppReceitas/app.py
@@ -1,17 +1,46 @@
-from flask import Flask
-from werkzeug.datastructures import RequestCacheControl
+from flask import Flask ,app , request
+from flask.json import jsonify
+import json
 
-app = Flask("Livro de Receitas")
+app = Flask(__name__)
 
-@app.route("/livros", methods=["GET"])
-def Livrodereceitas():
-    return {"Livro": "de receitas"}
+receitas = [
+    {
+        "Título": "Bolo01",
+        "Lista de ingredientes": [
+            "ingrediente01"
+            "ingrediente02"
+            "ingrediente03"
+        ],
 
-@app.route("/cadastro/Receitas", methods=["POST"])
-def cadastroReceitas():
-    body = Request.get_json()
-    print(body)
-    return body    
-app.run()
+        "modo": "modo01",
+        "Redimento": "Redimento01"
+        
+    },
+   
+]
+
+# @app.route("/cadastro1", methods=["GET"])
+# def Cadastro1():
+#    return receitas
+
+
+@app.route("/cadastro", methods=["POST","GET"])
+def Cadastro():
+    if request.method == "GET":
+        return jsonify(receitas)
+    elif request.method == "POST":
+        newcadastro = json.loads(request.data)
+        receitas.append(newcadastro)
+        return jsonify({
+            "menssagem" : "Cadastrado",
+            "newValue": newcadastro
+
+        })
+   
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
 
 
