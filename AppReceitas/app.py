4b3ab56455732cbd573ebbe6bce761a79346d3bf
ojuara-B <deividaraujope@gmail.com>
2021-10-17 14:58:26 -0300

    Arquivo Inicial

---
+++ AppReceitas/app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+app = Flask("Livro de Receitas")
+
+@app.route("/Livro_de_receitas", methods=["GET"])
+def Livro_de_receitas():
+    return {"Livro": "de receitas"}
+
+@app.route("/cadastra/Receitas", methods=["POST"])
+def cadastraReceitas():
+    return {"Receita": 1}    
+app.run()
+
